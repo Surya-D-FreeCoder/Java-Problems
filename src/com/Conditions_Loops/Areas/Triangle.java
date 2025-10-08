bf59a6507feb22cd16fcd23b0f07e8f130677305
Surya <suryarsk03@gmail.com>
2025-10-08 11:34:01 +0530

    Basic Java Problems has been added

---
+++ src/com/Conditions_Loops/Areas/Triangle.java
@@ -0,0 +1,11 @@
+package com.Conditions_Loops.Areas;
+
+public class Triangle {
+    public static void main(String[] args) {
+        int base = 5;
+        int height = 10;
+
+        double area = 0.5 * base * height;
+        System.out.println(area);
+    }
+}
---
+++ src/com/Conditions_Loops/SubractProductandSum.java
@@ -0,0 +1,29 @@
+// 1281 Leetcode ==> https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/description/
+
+package com.Conditions_Loops;
+
+import java.util.Scanner;
+
+public class SubractProductandSum  {
+        public static void main(String[] args) {
+            Scanner num = new Scanner(System.in);
+
+            System.out.print("Enter the number: ");
+            int number = num.nextInt();
+
+            int product = 1;
+            int sum = 0;
+            int temp = number;
+
+            while (temp > 0){
+                int last = temp % 10;
+                product = product * last;
+                sum = sum + last;
+                temp = temp / 10;
+            }
+
+            int result = product - sum;
+            System.out.println(result);
+        }
+    }
+
